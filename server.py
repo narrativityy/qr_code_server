1de066751d200872c3c488839c1931ce226d4f2f
narrativityy <jake_crowley@comcast.net>
2024-11-07 17:41:39 -0800

    added qr code route and functionality

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, send_file
+import qrcode
+import os
+
+app = Flask(__name__)
+
+@app.route('/send/<data>')
+def home(data):
+    if not search_files('./images', f'{data}.png'):
+        img = qrcode.make(data)
+        img.save(f'./images/{data}.png')
+        return send_file(f'./images/{data}.png', mimetype='image/png')
+    return send_file(f'./images/{data}.png', mimetype='image/png')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return jsonify(message=f"Hello, {name}!")
+
+def search_files(directory, filename):
+    for root, dirs, files in os.walk(directory):
+        if filename in files:
+            return True
+    return False
+
+if __name__ == '__main__':
+    app.run(debug=True)
